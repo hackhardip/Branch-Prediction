6ffb40fb49c704beab6e90341ee26f5e004dba64
hackhardip <47195513+hackhardip@users.noreply.github.com>
2020-03-26 12:25:58 -0400

    Initial Commit

---
+++ Machineproblem2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Machineproblem2
+//
+//  Created by Hardip Parmar on 3/26/20.
+//  Copyright © 2020 Hardip Parmar. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
